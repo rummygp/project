b5c4d808737daa2a59b8cac4971c382eb047fa87
egorummy <adam.mokrzycki22@gmail.com>
2025-10-07 12:34:42 +0200

    + RetryableException

--- src/main/java/com/example/project/config/GithubClientConfiguration.java
+++ src/main/java/com/example/project/config/GithubClientConfiguration.java
@@ -1,9 +1,13 @@
 package com.example.project.config;
 
 import com.example.project.exception.CustomErrorDecoder;
+import feign.Logger;
+import feign.Retryer;
 import feign.codec.ErrorDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class GithubClientConfiguration {
@@ -11,5 +15,15 @@ public class GithubClientConfiguration {
     @Bean
     public ErrorDecoder errorDecoder() {
         return new CustomErrorDecoder();
+    }
+
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.BASIC;
+    }
+
+    @Bean
+    public Retryer retryer() {
+        return new Retryer.Default(100L, TimeUnit.SECONDS.toMillis(3L), 5);
     }
 }
